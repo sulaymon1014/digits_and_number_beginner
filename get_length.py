e93b11229bb136d8308b7ae1e3cbba392b7a4f18
ZARIF ubuntu <uzbzarif@gmail.com>
2021-12-25 18:03:58 +0900

    add task

---
+++ get_length.py
@@ -0,0 +1,15 @@
+#get number of digits in an int?
+def get_length(num):
+    """
+    Get length of integer
+
+    Args:
+        num (int): integer to get length of
+
+    Returns:
+        int: length of integer
+    """
+    # return number of digits in integer
+    return 
+
+ 
---
+++ get_sum_digits.py
@@ -0,0 +1,13 @@
+#Find the sum of digits in an integer
+def get_sum_digits(num):
+    """
+    Get sum of digits in integer
+    
+    Args:
+        num (int): integer to get sum of digits of
+    
+    Returns:
+        int: sum of digits in integer
+    """
+    # return sum of digits in integer
+    return  
